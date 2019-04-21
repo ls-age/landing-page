5fdee665ac94ed3f99e2445379652703f68968ae
LukasHechenberger <admin@ls-age.com>
2019-04-21 12:58:46 +0200

    style: Disable eslint for built modules

--- src/client.js
+++ src/client.js
@@ -1,4 +1,4 @@
-import * as sapper from '../__sapper__/client.js';
+import * as sapper from '../__sapper__/client.js'; // eslint-disable-line import/no-unresolved
 
 sapper.start({
   target: document.querySelector('#sapper'),
